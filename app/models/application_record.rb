72f9ad11dc078c2a4666df2aaf2bd0e667ba4cc5
Santiago Pérez <santipego0001@gmail.com>
2026-04-04 20:35:14 -0500

    fix: add compatibility macro for excluded_from_tenants to fix boot error

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  # Compatibilidad con la implementación personalizada de multitenancy
+  class_attribute :excluded_from_tenants, default: false
 end
