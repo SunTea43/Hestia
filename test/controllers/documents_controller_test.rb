5b27cb7960cd856784e6a93ff96d5c21067472a5
Santiago Pérez <santipego0001@gmail.com>
2026-04-12 21:14:15 -0500

    test: add Document model and controller tests with new fields
    - Add tests for Document hierarchical structure
    - Add tests for Active Storage file attachment
    - Add tests for DocumentTemplate association
    - Add controller tests for parent_id, document_template_id, and file fields

--- test/controllers/documents_controller_test.rb
+++ test/controllers/documents_controller_test.rb
@@ -7,6 +7,7 @@ class DocumentsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:gestor)
     @document = documents(:one)
+    @template = document_templates(:one)
     sign_in @user
   end
 
@@ -23,5 +24,85 @@ class DocumentsControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
     get document_url(@document)
     assert_response :success
+  end
+
+  test "should create document with new fields" do
+    assert_difference "Document.count", 1 do
+      post documents_url, params: {
+        document: {
+          document_type_id: @document.document_type_id,
+          property_id: @document.property_id,
+          occupant_id: @document.occupant_id,
+          name: "New Document",
+          start_date: Date.today,
+          document_template_id: @template.id,
+          parent_id: nil
+        }
+      }
+    end
+
+    assert_redirected_to document_path(Document.last)
+  end
+
+  test "should create document with file" do
+    assert_difference "Document.count", 1 do
+      post documents_url, params: {
+        document: {
+          document_type_id: @document.document_type_id,
+          property_id: @document.property_id,
+          occupant_id: @document.occupant_id,
+          name: "Document with file",
+          start_date: Date.today,
+          file: fixture_file_upload("test.txt", "text/plain")
+        }
+      }
+    end
+
+    assert_redirected_to document_path(Document.last)
+    assert Document.last.file.attached?
+  end
+
+  test "should create document with parent" do
+    assert_difference "Document.count", 1 do
+      post documents_url, params: {
+        document: {
+          document_type_id: @document.document_type_id,
+          property_id: @document.property_id,
+          occupant_id: @document.occupant_id,
+          name: "Child Document",
+          start_date: Date.today,
+          parent_id: @document.id
+        }
+      }
+    end
+
+    assert_redirected_to document_path(Document.last)
+    assert_equal @document, Document.last.parent
+  end
+
+  test "should update document with new fields" do
+    patch document_url(@document), params: {
+      document: {
+        name: "Updated Name",
+        document_template_id: @template.id
+      }
+    }
+
+    assert_redirected_to document_path(@document)
+    @document.reload
+    assert_equal "Updated Name", @document.name
+    assert_equal @template.id, @document.document_template_id
+  end
+
+  test "should update document with file" do
+    patch document_url(@document), params: {
+      document: {
+        file: fixture_file_upload("test.txt", "text/plain")
+      }
+    }
+
+    assert_redirected_to document_path(@document)
+    @document.reload
+    assert @document.file.attached?
   end
 end
--- test/models/document_test.rb
+++ test/models/document_test.rb
@@ -1,7 +1,88 @@
 require "test_helper"
 
 class DocumentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @document = documents(:one)
+  end
+
+  test "should belong to document_template" do
+    assert_respond_to @document, :document_template
+  end
+
+  test "should have many children" do
+    assert_respond_to @document, :children
+  end
+
+  test "should belong to parent" do
+    assert_respond_to @document, :parent
+  end
+
+  test "should have one attached file" do
+    assert_respond_to @document, :file
+  end
+
+  test "should be valid without document_template" do
+    @document.document_template = nil
+    assert @document.valid?
+  end
+
+  test "should be valid without parent" do
+    @document.parent = nil
+    assert @document.valid?
+  end
+
+  test "should have hierarchical structure" do
+    parent = documents(:one)
+    child = Document.create!(
+      document_type: @document.document_type,
+      property: @document.property,
+      occupant: @document.occupant,
+      start_date: Date.today,
+      parent: parent
+    )
+
+    assert_equal parent, child.parent
+    assert_includes parent.children, child
+  end
+
+  test "should destroy children when destroyed" do
+    parent = documents(:one)
+    Document.create!(
+      document_type: @document.document_type,
+      property: @document.property,
+      occupant: @document.occupant,
+      start_date: Date.today,
+      parent: parent
+    )
+
+    assert_difference "Document.count", -2 do
+      parent.destroy
+    end
+  end
+
+  test "scope root should return documents without parent" do
+    parent = documents(:one)
+    child = Document.create!(
+      document_type: @document.document_type,
+      property: @document.property,
+      occupant: @document.occupant,
+      start_date: Date.today,
+      parent: parent
+    )
+
+    root_documents = Document.root
+    assert_includes root_documents, parent
+    assert_not_includes root_documents, child
+  end
+
+  test "should attach file" do
+    @document.file.attach(
+      io: StringIO.new("test content"),
+      filename: "test.txt",
+      content_type: "text/plain"
+    )
+
+    assert @document.file.attached?
+    assert_equal "test.txt", @document.file.filename.to_s
+  end
 end
